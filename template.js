60415b64c15111f87fa5a8cfe1fda579e699f26b
Li <linhit246@gmail.com>
2025-11-07 18:22:06 +0700

    Clean CDN

--- template.js
+++ template.js
@@ -1,10 +1,11 @@
+// version 1.0.0
+
 (function () {
-    const data = {
-      
-    };
-    const s = document.createElement("script");
-    s.type = "application/ld+json";
-    s.textContent = JSON.stringify(data);
-    document.head.appendChild(s);
-  })();
-  
+  const data = {
+
+  };
+  const s = document.createElement("script");
+  s.type = "application/ld+json";
+  s.textContent = JSON.stringify(data);
+  document.head.appendChild(s);
+})();
