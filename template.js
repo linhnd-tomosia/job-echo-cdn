bcfae926cc56648a5ed245efc2179dac999a7694
Li <linhit246@gmail.com>
2025-11-07 10:01:06 +0700

    Add template.js

---
+++ template.js
@@ -0,0 +1,10 @@
+(function () {
+    const data = {
+      
+    };
+    const s = document.createElement("script");
+    s.type = "application/ld+json";
+    s.textContent = JSON.stringify(data);
+    document.head.appendChild(s);
+  })();
+  
