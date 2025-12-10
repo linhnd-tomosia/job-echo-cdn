44f4b66f5e5313b2bf7de503a0ad1cceac87ecbd
Job Sync Service <jobsync@tomosia.com>
2025-12-10 17:27:21 +0700

    [Job Sync] Update Latest(20251210-102718) CDN - Công lý team - Ninja rùa!

---
+++ Công lý team/29531168-1767-42c2-918c-da6f080bdd6c/latest.js
@@ -0,0 +1,9 @@
+// version 1.0.2
+
+(function () {
+  const data = {"@type":"JobPosting","title":"Ninja rùa!","@context":"https://schema.org/","baseSalary":{"@type":"MonetaryAmount","value":{"@type":"QuantitativeValue","value":2,"unitText":"MONTH"},"currency":"USD"},"datePosted":"2025-12-10","description":"(Minimum 100 characters required)\nPlease include specific job responsibilities, ideal candidate profile, work style, etc.(Minimum 100 characters required)\nPlease include specific job responsibilities, ideal candidate profile, work style, etc.","directApply":false,"jobLocation":{"@type":"Place","address":{"@type":"PostalAddress","postalCode":"111-1111","addressRegion":"Aza","addressCountry":"US","addressLocality":"Skiu"}},"jobStartDate":"2025-12-10","validThrough":"2026-12-10","employmentType":"OTHER","salaryCurrency":"USD","jobImmediateStart":false,"applicationContact":{"name":"Takeore","@type":"ContactPoint","telephone":"09995555","contactType":"Phone"},"hiringOrganization":{"name":"Công lý team","@type":"Organization"},"experienceInPlaceOfEducation":false};
+  const s = document.createElement("script");
+  s.type = "application/ld+json";
+  s.textContent = JSON.stringify(data);
+  document.head.appendChild(s);
+})();
